ae6c1e5b4080b31207fc7c32498371fb2e768c90
Md.Raihanul Hasan <u2004031@student.cuet.ac.bd>
2026-03-02 07:39:39 +0600

    issues fixed related with source

--- mailer.py
+++ mailer.py
@@ -62,7 +62,7 @@ try:
           <hr>
 
           <p style="font-size:14px; text-align:center; color:#555;">
-            Source: {{'source'}}
+            {Source: ['source']}
           </p>
 
           <p style="text-align:center; font-size:12px; color:#999;">
